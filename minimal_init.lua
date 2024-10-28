823d72bf0c974c381a3a5def13bca0c989e6f746
Cathyprime <magdajaroszynska47@gmail.com>
2024-10-28 17:27:16 +0100

    refactor: vim.fn.system -> vim.system

--- minimal_init.lua
+++ minimal_init.lua
@@ -7,7 +7,7 @@ vim.o.background = "light"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    vim.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
