c5ffdfe5630d354f966aeefaf5b26e18598f5d5a
Cathyprime <magdajaroszynska47@gmail.com>
2024-10-28 20:55:57 +0100

    feat(lsp): split-win ALL

--- lua/cathy/config/lsp/hover.lua
+++ lua/cathy/config/lsp/hover.lua
@@ -13,6 +13,12 @@ local function new_buf(lines)
     vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
     vim.api.nvim_set_option_value("filetype", "markdown", { buf = buf })
     vim.api.nvim_buf_set_name(buf, "Documentation")
+    vim.keymap.set("n", "q", function()
+        local ok = pcall(vim.api.nvim_win_close, 0, false)
+        if not ok then
+            vim.cmd[[b#]]
+        end
+    end, { buffer = buf })
     return buf
 end
 
@@ -27,6 +33,7 @@ end
 local function display_in_split(buf)
     local target_win = nil
     local old_win = vim.api.nvim_get_current_win()
+    local old_cursor = vim.api.nvim_win_get_cursor(old_win)
 
     for win in pairs(vim.api.nvim_list_wins()) do
         local b = vim.api.nvim_win_get_buf(vim.fn.win_getid(win))
@@ -43,33 +50,42 @@ local function display_in_split(buf)
         vim.api.nvim_win_set_buf(target_win, buf)
     end
 
-    vim.api.nvim_set_option_value("conceallevel", 3, { win = target_win })
-    vim.api.nvim_set_option_value("concealcursor", "nvic", { win = target_win })
-    vim.api.nvim_set_option_value("relativenumber", false, { win = target_win })
-    vim.api.nvim_set_option_value("number", false, { win = target_win })
-    vim.api.nvim_set_option_value("statuscolumn", padding, { win = target_win })
-    vim.api.nvim_set_option_value("wrap", false, { win = target_win })
+    vim.api.nvim_set_option_value("conceallevel",   3,       { win = target_win })
+    vim.api.nvim_set_option_value("concealcursor",  "nvic",  { win = target_win })
+    vim.api.nvim_set_option_value("relativenumber", false,   { win = target_win })
+    vim.api.nvim_set_option_value("number",         false,   { win = target_win })
+    vim.api.nvim_set_option_value("statuscolumn",   padding, { win = target_win })
+    vim.api.nvim_set_option_value("wrap",           false,   { win = target_win })
+    vim.api.nvim_set_option_value("cursorline",     false,   { win = target_win })
+    vim.api.nvim_set_option_value("list",           false,   { win = target_win })
     vim.api.nvim_set_current_win(old_win)
+    vim.api.nvim_win_set_cursor(old_win, old_cursor)
     return target_win
 end
 
+local old_hover = vim.lsp.handlers.hover
+
 return {
-    hover = function(small_hover)
-        return function(_, result, ctx, config)
-            local lines = vim.split(result.contents.value:gsub("\n\n\n", "\n"):gsub("\n\n", "\n"), "\n")
-
-            if #lines <= 4 then
-                small_hover(nil, result, ctx, config)
-                return
-            end
-
-            if cache == nil or not vim.api.nvim_buf_is_valid(cache) then
-                cache = new_buf(lines)
-            else
-                set_lines(cache, lines)
-            end
-
-            resize(display_in_split(cache), #lines)
+    hover = function(_, result, ctx, config)
+        local lines
+        if result and result.contents and result.contents.value then
+            lines = vim.split(
+                result.contents.value:gsub("\n\n\n", "\n"):gsub("\n\n", "\n"),
+                "\n"
+            )
         end
+
+        if lines == nil then
+            vim.notify("No information available")
+            return
+        end
+
+        if cache == nil or not vim.api.nvim_buf_is_valid(cache) then
+            cache = new_buf(lines)
+        else
+            set_lines(cache, lines)
+        end
+
+        resize(display_in_split(cache), #lines)
     end
 }
