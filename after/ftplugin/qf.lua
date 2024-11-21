dfb7e3c6a8710e2fcb42a3eb4c7aa039e1b10cde
cathyprime <magdajaroszynska47@gmail.com>
2024-11-21 18:48:24 +0100

    feat(ftplugin): quickfix q close

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -11,13 +11,36 @@ local function cmd(command)
     end
 end
 
+local loc_qf = function(opts)
+    if vim.fn.getwininfo(vim.fn.win_getid())[1]['loclist'] == 1 then
+        return opts.on_loc()
+    else
+        return opts.on_qf()
+    end
+end
+
 -- vim.keymap.set("n", "<", cmd("colder"), { buffer = true, silent = true, expr = true })
 -- vim.keymap.set("n", ">", cmd("cnewer"), { buffer = true, silent = true, expr = true })
 vim.keymap.set("n", "o", function()
-    if vim.fn.getwininfo(vim.fn.win_getid())[1]['loclist'] == 1 then
-        return "<cr><cmd>lclose<cr>"
-    end
-    return "<cr><cmd>cclose<cr>"
+    return loc_qf({
+        on_qf = function()
+            return "<cr><cmd>cclose<cr>"
+        end,
+        on_loc = function()
+            return "<cr><cmd>lclose<cr>"
+        end
+    })
+end, { buffer = true, silent = true, expr = true })
+
+vim.keymap.set("n", "q", function()
+    return loc_qf({
+        on_qf = function()
+            return "<cmd>cclose<cr>"
+        end,
+        on_loc = function()
+            return "<cmd>lclose<cr>"
+        end
+    })
 end, { buffer = true, silent = true, expr = true })
 
 vim.keymap.set("x", "R", ":Reject<cr>")
