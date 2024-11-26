6d24b9b0db5cec9a8654ab751b33b31b184e9568
Cathyprime <magdajaroszynska47@gmail.com>
2024-11-26 15:13:02 +0100

    fix(ftplugin): html settings

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,4 +1,4 @@
 local buf = vim.api.nvim_get_current_buf()
-vim.api.nvim_set_option_value("tabstop", 2, { buf = buf })
-vim.api.nvim_set_option_value("shiftwidth", 2, { buf = buf })
-vim.api.nvim_set_option_value("expandtab", false, { buf = buf })
+vim.api.nvim_set_option_value("tabstop", 4, { buf = buf })
+vim.api.nvim_set_option_value("shiftwidth", 4, { buf = buf })
+vim.api.nvim_set_option_value("expandtab", true, { buf = buf })
