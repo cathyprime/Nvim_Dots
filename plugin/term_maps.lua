f4ab99b220dc84305cadf4bb65940995af846f1e
cathyprime <magdajaroszynska47@gmail.com>
2024-12-01 13:06:23 +0100

    fix(term): shift space issue

--- plugin/term_maps.lua
+++ plugin/term_maps.lua
@@ -1,8 +1,12 @@
 vim.api.nvim_create_autocmd("TermOpen", {
     callback = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        local map = require("cathy.utils").map_gen({ buffer = bufnr, silent = true })
+        local map = require("cathy.utils").map_gen({
+            buffer = vim.api.nvim_get_current_buf(),
+            silent = true
+        })
         map("t", "<esc><esc>", [[<c-\><c-n>]])
         map("t", "<m-w>", [[<c-\><c-n><c-w>w]])
+        map("t", "<s-space>", "<space>")
+        map("t", "<c-bs>", "<c-w>")
     end,
 })
