90e5f689f47afa99f366e062dbb316068b409742
cathyprime <magdajaroszynska47@gmail.com>
2024-12-04 18:33:17 +0100

    fix(ftplugin): propa c++ indent

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,9 +1,10 @@
 vim.opt_local.expandtab = true
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
+vim.opt_local.indentkeys = "0{,0},0),0],0#,!^F,o,O,e"
 vim.api.nvim_create_autocmd("BufEnter", {
     once = true,
     callback = function()
-        vim.opt_local.indentexpr = 'v:lua.require("cathy.utils.indent").cpp_indent()'
+        vim.opt_local.indentexpr = 'v:lua.require("cathy.utils.indent").cpp_indent(v:lnum)'
     end,
 })
--- lua/cathy/utils/indent.lua
+++ lua/cathy/utils/indent.lua
@@ -1,15 +1,53 @@
+local function getline(lnum)
+    return vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, false)[1] or ""
+end
+
+---@param lnum integer
+---@return integer
+local function get_indentcols_at_line(lnum)
+    local _, indentcols = getline(lnum):find "^%s*"
+    return indentcols or 0
+end
+
+---@param root TSNode
+---@param lnum integer
+---@param col? integer
+---@return TSNode
+local function get_first_node_at_line(root, lnum, col)
+    col = col or get_indentcols_at_line(lnum)
+    return root:descendant_for_range(lnum - 1, col, lnum - 1, col + 1)
+end
+
+local valid_nodes = {
+    access_specifier = 2,
+    field_initializer_list = -2,
+}
+
 return {
-    cpp_indent = function()
+    cpp_indent = function(lnum)
         local ts_utils = require("nvim-treesitter.ts_utils")
+        local indent = require("nvim-treesitter.indent")
 
         local node = ts_utils.get_node_at_cursor()
-        if not node then return 0 end
-
-        local node_type = node:type()
-        if node_type == "access_specifier" then
-            return vim.fn.shiftwidth() / 2
+        if not node then
+            return -1
         end
 
-        return require("nvim-treesitter").get_node_indent(node)
+        local node = get_first_node_at_line(node, lnum)
+        if not node then
+            return -1
+        end
+
+        local indt = indent.get_indent(lnum)
+        local p = node:parent()
+
+        if p then
+            local key = valid_nodes[p:type()]
+            if key then
+                indt = indt + key
+            end
+        end
+
+        return indt
     end
 }
