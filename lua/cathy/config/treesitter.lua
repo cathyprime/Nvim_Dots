84cf32d5bcf31834db22d168e9031dda2b5e8073
cathyprime <magdajaroszynska47@gmail.com>
2024-12-06 16:17:44 +0100

    feat(treesitter): disable in large files (>100kb)

--- lua/cathy/config/treesitter.lua
+++ lua/cathy/config/treesitter.lua
@@ -1,3 +1,11 @@
+local disable_func = function(lang, buf)
+    local max_filesize = 100 * 1024 -- 100kb
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    if ok and stats and stats.size > max_filesize then
+        return true
+    end
+end
+
 ---@diagnostic disable-next-line
 require("nvim-treesitter.configs").setup({
     auto_install = true,
@@ -19,13 +27,16 @@ require("nvim-treesitter.configs").setup({
     },
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
+        disable = disable_func,
+        additional_vim_regex_highlighting = true,
     },
     indent = {
         enable = true,
+        disable = disable_func,
     },
     incremental_selection = {
         enable = true,
+        disable = disable_func,
         keymaps = {
             node_incremental = "<c-w>",
             node_decremental = "<c-e>",
