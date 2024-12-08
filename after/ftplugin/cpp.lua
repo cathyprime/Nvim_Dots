de0c753e06d30c48a3323972336f1d942956ec0e
cathyprime <magdalena.mjaroszynska@gmail.com>
2024-12-08 14:03:31 +0100

    feat(ftplugin): cpp formatprg

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,6 +1,7 @@
 vim.opt_local.expandtab = true
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
+vim.bo.formatprg = "clang-format -style=file:" .. os.getenv("HOME") .. "/.config/.clang-format"
 vim.opt_local.indentkeys = "0{,0},0),0],0#,!^F,o,O,e"
 vim.api.nvim_create_autocmd("BufEnter", {
     once = true,
