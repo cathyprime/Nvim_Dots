40657e08478bce8a6dbcccad12d1ffdf8da792e2
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-01-04 01:43:13 +0100

    fix(scala): use blink

--- after/ftplugin/scala.lua
+++ after/ftplugin/scala.lua
@@ -7,7 +7,7 @@ metals_config.settings = {
     showImplicitArguments = true,
     excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
-metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+metals_config.capabilities = require("blink.cmp").get_lsp_capabilities()
 metals_config.on_attach = function(client, bufnr)
     require("metals").setup_dap()
     local dap = require("dap")
