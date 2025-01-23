4301b65b55de3a9b08f9d19a80d0ae4b480dd35e
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-01-23 23:24:51 +0100

    feat(mini): remove outer scope keybind

--- lua/cathy/plugin/mini.lua
+++ lua/cathy/plugin/mini.lua
@@ -14,6 +14,32 @@ local minis = {
                 end
             end
         })
+        vim.keymap.set("o", "o", function()
+            local operator = vim.v.operator
+            if operator == "d" then
+                local scope = MiniIndentscope.get_scope()
+                local top = scope.border.top
+                local bottom = scope.border.bottom
+                local row, col = vim.api.nvim_win_get_cursor(0)
+                local move = ""
+                if row == bottom then
+                    move = "k"
+                elseif row == top then
+                    move = "j"
+                end
+                local ns = vim.api.nvim_create_namespace("border")
+                vim.api.nvim_buf_add_highlight(0, ns, "Substitute", top - 1, 0, -1)
+                vim.api.nvim_buf_add_highlight(0, ns, "Substitute", bottom - 1, 0, -1)
+                vim.defer_fn(function()
+                    vim.api.nvim_buf_set_text(0, top - 1, 0, top - 1, -1, {})
+                    vim.api.nvim_buf_set_text(0, bottom - 1, 0, bottom - 1, -1, {})
+                    vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+                end, 150)
+                return "<esc>" .. move
+            else
+                return "o"
+            end
+        end, { expr = true })
     end,
 
     operators = function()
