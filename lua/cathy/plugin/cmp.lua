430d90716ea9dcd59bc87bbb3fe1be26395fabcf
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-03-05 23:35:47 +0100

    feat(snippets): integrate vim.snippets for lsp snippets

--- lua/cathy/plugin/cmp.lua
+++ lua/cathy/plugin/cmp.lua
@@ -55,16 +55,6 @@ return {
                 ["<C-d>"] = { "scroll_documentation_down", "fallback" },
                 ["<C-u>"] = { "scroll_documentation_up", "fallback" },
             },
-            snippets = {
-                expand = function(snippet) require("luasnip").lsp_expand(snippet) end,
-                active = function(filter)
-                    if filter and filter.direction then
-                        return require("luasnip").jumpable(filter.direction)
-                    end
-                    return require("luasnip").in_snippet()
-                end,
-                jump = function(direction) require("luasnip").jump(direction) end,
-            },
             appearance = {
                 use_nvim_cmp_as_default = false,
                 nerd_font_variant = "mono",
