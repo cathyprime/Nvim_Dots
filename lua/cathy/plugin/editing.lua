507d8c1f8c8696004ff76d44e028fef5a505412d
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-03-05 20:35:27 +0100

    feat(mc): use new apis

--- lua/cathy/plugin/editing.lua
+++ lua/cathy/plugin/editing.lua
@@ -18,7 +18,7 @@ return {
             },
             surrounds = {
                 T = {
-                    add = function()
+                    add = function ()
                         local user_input = require("nvim-surround.config").get_input("Enter the HTML tag: ")
                         if user_input then
                             local element = user_input:match("^<?([^%s>]*)")
@@ -28,13 +28,13 @@ return {
                             return { { "<" .. open .. ">" }, { "</" .. close .. ">" } }
                         end
                     end,
-                    find = function()
+                    find = function ()
                         return require("nvim-surround.config").get_selection({ motion = "at" })
                     end,
                     delete = "^(%b<>)().-(%b<>)()$",
                     change = {
                         target = "^<([^%s<>]*)().-([^/]*)()>$",
-                        replacement = function()
+                        replacement = function ()
                             local user_input = require("nvim-surround.config").get_input("Enter the HTML tag: ")
                             if user_input then
                                 local element = user_input:match("^<?([^%s>]*)")
@@ -47,13 +47,13 @@ return {
                     },
                 },
                 t = {
-                    add = function()
+                    add = function ()
                         local type = require("nvim-surround.config").get_input("Enter the TYPE: ")
                         if type then
                             return { { type .. "<" }, { ">" }}
                         end
                     end,
-                    find = function()
+                    find = function ()
                         local c = require("nvim-surround.config")
                         if vim.g.loaded_nvim_treesitter then
                             local selection = c.get_selection({
@@ -71,7 +71,7 @@ return {
                     delete = "^(.-%<)().-(%>)()$",
                     change = {
                         target = "^.-([%w_]+)()%<.-%>()()$",
-                        replacement = function()
+                        replacement = function ()
                             local type = require("nvim-surround.config").get_input("Enter the type name: ")
                             if type then
                                 return { { type }, { "" } }
@@ -85,49 +85,57 @@ return {
     {
         "monaqa/dial.nvim",
         event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-        config = function()
+        config = function ()
             local dial = require("cathy.config.dial")
             require("dial.config").augends:register_group(dial.register_group)
             require("dial.config").augends:on_filetype(dial.on_filetype)
 
-            local mani = function(...)
+            local mani = function (...)
                 pcall(require("dial.map").manipulate, ...)
             end
-            vim.keymap.set("n", "<c-a>",  function() mani("increment", "normal")         end)
-            vim.keymap.set("n", "<c-x>",  function() mani("decrement", "normal")         end)
-            vim.keymap.set("n", "g<c-a>", function() mani("increment", "gnormal")        end)
-            vim.keymap.set("n", "g<c-x>", function() mani("decrement", "gnormal")        end)
-            vim.keymap.set("v", "<c-a>",  function() mani("increment", "visual")         end)
-            vim.keymap.set("v", "<c-x>",  function() mani("decrement", "visual")         end)
-            vim.keymap.set("v", "g<c-a>", function() mani("increment", "gvisual")        end)
-            vim.keymap.set("v", "g<c-x>", function() mani("decrement", "gvisual")        end)
-            vim.keymap.set("n", "<c-g>",  function() mani("increment", "normal", "case") end)
+            vim.keymap.set("n", "<c-a>",  function () mani("increment", "normal")         end)
+            vim.keymap.set("n", "<c-x>",  function () mani("decrement", "normal")         end)
+            vim.keymap.set("n", "g<c-a>", function () mani("increment", "gnormal")        end)
+            vim.keymap.set("n", "g<c-x>", function () mani("decrement", "gnormal")        end)
+            vim.keymap.set("v", "<c-a>",  function () mani("increment", "visual")         end)
+            vim.keymap.set("v", "<c-x>",  function () mani("decrement", "visual")         end)
+            vim.keymap.set("v", "g<c-a>", function () mani("increment", "gvisual")        end)
+            vim.keymap.set("v", "g<c-x>", function () mani("decrement", "gvisual")        end)
+            vim.keymap.set("n", "<c-g>",  function () mani("increment", "normal", "case") end)
         end
     },
     {
         "jake-stewart/multicursor.nvim",
         branch = "1.0",
-        config = function()
+        config = function ()
             local mc = require("multicursor-nvim")
             mc.setup()
 
-            vim.keymap.set("n", "<c-n>",   function() mc.addCursor("*")  end)
-            vim.keymap.set("n", "<c-s-n>", function() mc.addCursor("#")  end)
-            vim.keymap.set("n", "<c-s>",   function() mc.skipCursor("*") end)
-            vim.keymap.set("x", "<c-n>",   function() mc.addCursor("*")  end)
-            vim.keymap.set("x", "<c-s-n>", function() mc.addCursor("#")  end)
-            vim.keymap.set("x", "<c-s>",   function() mc.skipCursor("#") end)
+            vim.keymap.set("n", "<c-n>",   function () mc.addCursor("*")  end)
+            vim.keymap.set("n", "<c-s-n>", function () mc.addCursor("#")  end)
+            vim.keymap.set("n", "<c-s>",   function () mc.skipCursor("*") end)
+            vim.keymap.set("x", "<c-n>",   function () mc.addCursor("*")  end)
+            vim.keymap.set("x", "<c-s-n>", function () mc.addCursor("#")  end)
+            vim.keymap.set("x", "<c-s>",   function () mc.skipCursor("#") end)
             vim.keymap.set("n", "<leader>gv", mc.restoreCursors)
 
             vim.keymap.set("x", "<c-q>", mc.visualToCursors)
             vim.keymap.set("x", "m",     mc.matchCursors)
             vim.keymap.set("x", "M",     mc.splitCursors)
 
-            vim.keymap.set("n", "ga", mc.alignCursors)
-            -- vim.keymap.set("x", "I",  mc.insertVisual)
-            -- vim.keymap.set("x", "A",  mc.appendVisual)
+            vim.keymap.set({ "n", "x" }, "<c-i>", mc.jumpForward)
+            vim.keymap.set({ "n", "x" }, "<c-o>", mc.jumpBackward)
 
-            vim.keymap.set("n", "<esc>", function()
+            vim.keymap.set("x", "ga", mc.operator)
+            vim.keymap.set("n", "ga", function ()
+                if mc.hasCursors() then
+                    mc.alignCursors()
+                else
+                    mc.operator()
+                end
+            end)
+
+            vim.keymap.set("n", "<esc>", function ()
                 if mc.hasCursors() then
                     mc.clearCursors()
                 else
@@ -138,7 +146,7 @@ return {
     },
     {
         "chaoren/vim-wordmotion",
-        init = function()
+        init = function ()
             vim.g.wordmotion_extra = {
                 "\\%([[:upper:]][[:lower:]]\\+\\)\\+",
                 "\\<[[:upper:]]\\@![[:lower:]]\\+\\%([[:upper:]][[:lower:]]\\+\\)\\+",
@@ -155,7 +163,7 @@ return {
     {
         "cathyprime/substitute.nvim",
         build = "make",
-        config = function()
+        config = function ()
             require("substitute")
             vim.keymap.set("n", "gs", "<Plug>(substitute)")
             vim.keymap.set("n", "gss", "<Plug>(substitute-linewise)")
