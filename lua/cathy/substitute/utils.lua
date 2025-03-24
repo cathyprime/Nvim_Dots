d703eebcc719679374395ff81632f327614dc0e1
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-03-24 22:43:32 +0100

    feat(substitute): finish utils

--- lua/cathy/substitute/utils.lua
+++ lua/cathy/substitute/utils.lua
@@ -1,24 +1,28 @@
----@class Str_Opts
----@field uppercase boolean
----@field lowercase boolean
----@field underscore boolean
----@field dash boolean
----@field dot boolean
----@field slash boolean
----@field space boolean
-
----@param str string
----@return Str_Opts
 local str_to_opts = function (str)
     return {
         uppercase  = str:match "[A-Z]" ~= nil,
         lowercase  = str:match "[a-z]" ~= nil,
-        underscore = str:match "_" ~= nil,
-        dash       = str:match "-" ~= nil,
-        dot        = str:match "%." ~= nil,
-        slash      = str:match "/" ~= nil,
-        space      = str:match " " ~= nil,
+        underscore = str:match "_"     ~= nil,
+        dash       = str:match "-"     ~= nil,
+        dot        = str:match "%."    ~= nil,
+        slash      = str:match "/"     ~= nil,
+        space      = str:match " "     ~= nil,
     }
+end
+
+local tbl_partition = function (tbl, pred)
+    local p1 = {}
+    local p2 = {}
+
+    for key, value in pairs(tbl) do
+        if (pred(key)) then
+            p1[key] = value
+        else
+            p2[key] = value
+        end
+    end
+
+    return p1, p2
 end
 
 local starts_capital = function (str)
@@ -35,36 +39,183 @@ local all_words_with_capital = function (sep)
     end
 end
 
+local first_upper = function (it)
+    return (it:gsub("^%l", string.upper))
+end
+
+local split_capitals = function (str)
+    str = str:gsub("([A-Z])", " %1")
+    str = str:gsub("^%s+", "")
+    return vim.iter(str:gmatch("%S+")):fold({}, function (acc, it)
+        table.insert(acc, it)
+        return acc
+    end)
+end
+
+local simple_split_gen = function (sep)
+    return function (str)
+        return vim.split(str, sep, { plain = true, trimempty = true })
+    end
+end
+
+local simple_join_gen = function (sep)
+    return function (parts)
+        return table.concat(parts, sep)
+    end
+end
+
+local modifier_split_gen = function (split, mod)
+    return function (str)
+        return vim.iter(split(str)):map(mod):totable()
+    end
+end
+
+local modifier_join_gen = function (join, mod)
+    return function (parts)
+        return join(vim.iter(parts):map(mod):totable())
+    end
+end
+
 local case = {
     ada = {
-        "lowercase", "uppercase", "underscore",
-        starts_capital, all_words_with_capital("_")
+        rules = {
+            "lowercase", "uppercase", "underscore",
+            starts_capital, all_words_with_capital "_",
+        },
+        to = modifier_join_gen(simple_join_gen "_", first_upper),
+        from = modifier_split_gen(simple_split_gen "_", string.lower)
     },
     camel = {
-        "lowercase", "uppercase", starts_small
+        rules = {
+            "lowercase", "uppercase", starts_small
+        },
+        to = function (parts)
+            local t = vim.iter(parts):skip(1):map(first_upper):totable()
+            table.insert(t, 1, parts[1])
+            return table.concat(t)
+        end,
+        from = modifier_split_gen(split_capitals, string.lower)
     },
     dot = {
-        "lowercase", "uppercase", "dot",
+        rules = {
+            "lowercase", "dot",
+        },
+        to = simple_join_gen ".",
+        from = simple_split_gen "."
     },
     kebab = {
-        "dash", "lowercase"
+        rules = {
+            "dash", "lowercase"
+        },
+        to = simple_join_gen "-",
+        from = simple_split_gen "-"
     },
     pascal = {
-        "lowercase", "uppercase", starts_capital
+        rules = {
+            "lowercase", "uppercase", starts_capital,
+        },
+        to = modifier_join_gen(simple_join_gen(), first_upper),
+        from = modifier_split_gen(split_capitals, string.lower)
     },
     path = {
-        "lowercase", "slash",
+        rules = {
+            "lowercase", "slash",
+        },
+        to = simple_join_gen "/",
+        from = simple_split_gen "/"
     },
     screaming_snake = {
-        "uppercase", "underscore"
+        rules = {
+            "uppercase", "underscore"
+        },
+        to = modifier_join_gen(simple_join_gen "_", string.upper),
+        from = modifier_split_gen(simple_split_gen "_", string.lower)
     },
     snake = {
-        "lowercase", "underscore"
+        rules = {
+            "lowercase", "underscore"
+        },
+        to = simple_join_gen "_",
+        from = simple_split_gen "_"
     },
     space = {
-        "lowercase", "space",
+        rules = {
+            "lowercase", "space",
+        },
+        to = simple_join_gen " ",
+        from = simple_split_gen " "
     },
     title = {
-        "uppercase", "lowercase", starts_capital, all_words_with_capital("-")
-    }
+        rules = {
+            "uppercase", "lowercase", "dash",
+            starts_capital, all_words_with_capital "-"
+        },
+        to = modifier_join_gen(simple_join_gen "-", first_upper),
+        from = modifier_split_gen(simple_split_gen "-", string.lower)
+    },
+}
+
+local case_of = function (str)
+    local str_opts = str_to_opts(str)
+    for case, v in pairs(case) do
+        local names = vim.iter(v.rules)
+            :filter(function (v) return type(v) == "string" end)
+            :totable()
+        local funcs = vim.iter(v.rules)
+            :filter(function (v) return type(v) == "function" end)
+
+        local must, must_not = tbl_partition(str_opts, function (it) return vim.tbl_contains(names, it) end)
+
+        local must_satisfied = vim.iter(must):all(function (_, value) return value end)
+        local must_not_satisfied = vim.iter(must_not):all(function (_, value) return not value end)
+        local funcs_satisfied = true
+        if funcs:peek() ~= nil then
+            funcs_satisfied = funcs:all(function (f) return f(str) end)
+        end
+
+        if must_satisfied and must_not_satisfied and funcs_satisfied then
+            return case
+        end
+    end
+
+    return nil
+end
+
+local permutations = function (str)
+    local caseof = case_of(str)
+    if caseof == nil then
+        return nil
+    end
+    local parts = case[caseof].from(str)
+
+    local result = {}
+    for _, tbl in pairs(case) do
+        table.insert(result, tbl.to(parts))
+    end
+    return result
+end
+
+return {
+    caseof = case_of,
+    permutations = permutations,
+    str_to_parts = function (str)
+        local str_case = case_of(str)
+        if str_case == nil then
+            return nil
+        end
+        return case[case_of(str)].to(str)
+    end,
+    convert = function (str, str_case)
+        local case_of_str = case_of(str)
+        if str_case == nil then
+            return nil
+        end
+        return case[str_case].to(case[case_of_str].from(str))
+    end,
+    parts_to_str = function (parts, str_case)
+        return case[str_case].from(parts)
+    end,
+    make_regex = function (str)
+        return string.format([[\v\C(%s)]], table.concat(permutations(str), "|"))
+    end
 }
