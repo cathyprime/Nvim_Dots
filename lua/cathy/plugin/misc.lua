be8c972bd5ec1ae3331cf504c33cdc4ccdb29f6b
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-04-20 10:33:25 +0200

    feat(misc): use flash

--- lua/cathy/plugin/misc.lua
+++ lua/cathy/plugin/misc.lua
@@ -136,4 +136,18 @@ return {
             },
         }
     },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {
+            modes = {
+                search = { enabled = true },
+                char = { enabled = false }
+            }
+        },
+        keys = {
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+    }
 }
