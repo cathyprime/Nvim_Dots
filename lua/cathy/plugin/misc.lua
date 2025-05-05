04fe4a750e4b6021ffd92ebf7cf1740423ead865
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-05-05 11:28:48 +0200

    remove: auto-cmdheight

--- lua/cathy/plugin/misc.lua
+++ lua/cathy/plugin/misc.lua
@@ -73,15 +73,5 @@ return {
         keys = {
             { "gX", "<cmd>Browse<cr>" }
         },
-    },
-    {
-        "jake-stewart/auto-cmdheight.nvim",
-        lazy = false,
-        opts = {
-            max_lines = 5,
-            duration = 3,
-            remove_on_key = true,
-            clear_always = false,
-        }
     }
 }
