e25f5e0aefe6b9385511b48bafa725a31e8bd265
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-11-03 00:08:39 +0100

    feat: build blink in terminal

--- init.lua
+++ init.lua
@@ -47,12 +47,9 @@ vim.api.nvim_create_autocmd("FileType", {
 
 local function build_blink(params)
     vim.notify("Building blink.cmp", vim.log.levels.INFO)
-    local obj = vim.system({ "cargo", "build", "--release" }, { cwd = params.path }):wait()
-    if obj.code == 0 then
-        vim.notify("Building blink.cmp done", vim.log.levels.INFO)
-    else
-        vim.notify("Building blink.cmp failed", vim.log.levels.ERROR)
-    end
+    vim.fn.jobstart({ "cargo", "build", "--release" }, {
+        cwd = params.path
+    })
 end
 
 local plugins = {
@@ -90,9 +87,8 @@ local plugins = {
         source = "nvim-treesitter/nvim-treesitter",
         checkout = "main",
         hooks = {
-            post_install = function () vim.cmd "TSUpdate" end,
-            post_checkout = function () vim.cmd "TSUpdate" end
-        }
+            post_checkout = function() vim.cmd "TSUpdate" end
+        },
     },
     {
         source = "nvim-treesitter/nvim-treesitter-textobjects",
