c71d481c77e18d08d252fb67d926d6efbcbbe7e6
Magdalena <magdalena.mjaroszynska@gmail.com>
2025-11-12 13:04:00 +0100

    fix(ftplugin): gitcommit keymaps

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,4 +1,4 @@
 vim.b.minitrailspace_disable = true
-vim.keymap.set("n", "<c-c>", "<cmd>wq<cr>")
-vim.keymap.set("n", "<c-k>", "<cmd>q!<cr>")
+vim.keymap.set("n", "<c-c>", "<cmd>wq<cr>", { buffer = true })
+vim.keymap.set("n", "<c-k>", "<cmd>q!<cr>", { buffer = true })
 vim.cmd "startinsert"
