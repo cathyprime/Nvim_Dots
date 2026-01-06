2d3ff31b0961a1b20a0e0c3c7d4e7e5030fe502f
cathy64_t <magdalena.mjaroszynska@gmail.com>
2026-01-06 19:39:10 +0100

    fix(mini.pick): open in correct window

--- lua/cathy/utils/mini/man.lua
+++ lua/cathy/utils/mini/man.lua
@@ -3,6 +3,7 @@ local pick = require("mini.pick")
 return function (opts)
     local items = {}
 
+    local current_win = vim.api.nvim_get_current_win()
     vim.system(
         { "man", "-k", "." },
         { text = true },
@@ -22,7 +23,6 @@ return function (opts)
                 end
             end
 
-
             vim.schedule(function()
                 pick.start(vim.tbl_deep_extend("force", {
                     window = {
@@ -32,9 +32,17 @@ return function (opts)
                         name = "Man pages",
                         items = items,
                         choose = function(item)
-                            if item then
-                                vim.cmd("Man " .. item.page)
+                            if not item then
+                                return
                             end
+                            local target = MiniPick.get_picker_state().windows.target
+                            if target ~= current_win then
+                                vim.api.nvim_win_call(target, function ()
+                                    vim.cmd("hide Man " .. item.page)
+                                    return
+                                end)
+                            end
+                            vim.cmd("Man " .. item.page)
                         end,
                     },
                 }, opts))
